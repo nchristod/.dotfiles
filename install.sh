573e0ffc248c82205ae745f25b81d107d300aae3
Christodoulopoulos Nikolaos <nchristod@gmail.com>
2016-08-19 15:20:33 +0300

    Better Bash prompt with git status and minor additions.

--- install.sh
+++ install.sh
@@ -7,15 +7,19 @@ ln -sv ~/.dotfiles/bash/bashrc ~/.bashrc
 ln -sv ~/.dotfiles/bash/bash_aliases ~/.bash_aliases
 ln -sv ~/.dotfiles/bash/bash_helpers ~/.bash_helpers
 ln -sv ~/.dotfiles/bash/bash_logout ~/.bash_logout
+ln -sv ~/.dotfiles/bash/bash_prompt ~/.bash_prompt
+ln -sv ~/.dotfiles/bash/git_prompt.sh ~/.git_prompt.sh
 
 echo "Linking Git configs.."
 ln -sv ~/.dotfiles/git/gitconfig ~/.gitconfig
 
 echo "Linking Vim configs.."
-ln -sv ~/.dotfiles/vimfiles/vim ~/.vim
+ln -nsv ~/.dotfiles/vimfiles/vim ~/.vim
 ln -sv ~/.dotfiles/vimfiles/vimrc ~/.vimrc
 ln -sv ~/.dotfiles/vimfiles/gvimrc ~/.gvimrc
 
 echo "Linking misc files.."
 #ln -sv ~/.dotfiles/gdbinit ~/.gdbinit
-ln -sv ~/.dotfiles/fonts ~/.fonts
+ln -nsv ~/.dotfiles/fonts ~/.fonts
+ln -sv ~/.dotfiles/TBox.conkyrc ~/.config/conky/TBox.conkyrc
+ln -sv ~/.dotfiles/cpu_tbox.lua ~/.config/conky/LUA/cpu_tbox.lua
