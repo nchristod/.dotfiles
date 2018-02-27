ac7e291c54372a0191a9747890a12446d90a925a
Christodoulopoulos Nikolaos <nchristod@gmail.com>
2018-02-28 00:58:20 +0200

    xmonad: Try out xmobar

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -6,8 +6,13 @@ import XMonad.Util.EZConfig(additionalKeys)
 import System.IO
 
 main = do
-  xmonad $ def {
-         manageHook = manageDocks <+> manageHook def,
-         layoutHook = avoidStruts  $  layoutHook def,
-         modMask = mod4Mask -- Mod is Super Key
-         }
+  xmproc <- spawnPipe "xmobar /home/drugsha/.xmonad/.xmobarrc"
+  xmonad $ def
+      { manageHook = manageDocks <+> manageHook def
+      , layoutHook = avoidStruts  $  layoutHook def
+      , modMask = mod4Mask -- Mod is Super Key
+      , logHook = dynamicLogWithPP xmobarPP
+                      { ppOutput = hPutStrLn xmproc
+                      , ppTitle = xmobarColor "green" "" . shorten 50
+                      }
+      }
