8f979a4c4543f340aa87b485ce556d59d17747f5
Christodoulopoulos Nikolaos <nchristod@gmail.com>
2018-11-01 21:45:18 +0200

    xmonad: Add scratchpads

--- xmonad/xmonad.hs
+++ xmonad/xmonad.hs
@@ -2,8 +2,10 @@ import XMonad
 import XMonad.Hooks.DynamicLog
 import XMonad.Hooks.ManageDocks
 import XMonad.Hooks.ManageHelpers
+import XMonad.Hooks.EwmhDesktops
 import XMonad.Util.Run(spawnPipe)
 import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Prompt
 import System.IO
 
 import Graphics.X11.ExtraTypes.XF86
@@ -36,18 +38,23 @@ main = do
       , layoutHook = myLayoutHook
       , workspaces = myWorkspaces
       , modMask = mod4Mask -- Mod is Super Key
-      , handleEventHook = handleEventHook def <+> docksEventHook
+      , handleEventHook = handleEventHook def <+> docksEventHook <+> fullscreenEventHook
       , logHook = dynamicLogWithPP xmobarPP
                       { ppOutput = hPutStrLn xmproc
                       , ppTitle = xmobarColor "green" "" . shorten 50
                       }
       }
       `additionalKeys`
-          [ ((0, xF86XK_AudioRaiseVolume), spawn "amixer -q sset Master 2%+")
-          , ((0, xF86XK_AudioLowerVolume), spawn "amixer -q sset Master 2%-")
+          [ ((0, xF86XK_AudioRaiseVolume), spawn "amixer -q sset Master 5%+")
+          , ((0, xF86XK_AudioLowerVolume), spawn "amixer -q sset Master 5%-")
           , ((0, xF86XK_AudioMute), spawn "amixer set Master toggle")
           -- , ((0, xF86XK_AudioMicMute), spawn "amixer set Capture toggle")
           , ((0, 0x1008FFB2), spawn "amixer set Capture toggle")
+          -- ]
+          -- ++
+          -- scratchpads
+          ,((mod4Mask .|. controlMask, xK_k), namedScratchpadAction myScratchpads "keepassxc")
+          ,((mod4Mask, xK_z), namedScratchpadAction myScratchpads "zeal")
           ]
 
 -- Borders
@@ -59,23 +66,25 @@ myFocusedBorderColor = "#306EFF"
 
 -- Workspaces
 myWorkspaces :: [WorkspaceId]
-myWorkspaces = ["1", "2:work", "3:doc", "4:IM", "5:vm", "6:mov", "7:gs", "8:music", "9:gimp"]
+myWorkspaces = ["1", "2:work", "3:doc", "4:IM", "5:vm", "6:mov", "7:gs", "8:music", "9:gimp"] ++ ["NSP"]
 
 -- Scratchpads
-
-isKeepass = className =? "keepassx"
+isKeepass = className =? "keepassxc"
 isZeal = className =? "Zeal"
 
 myScratchpads =
-    [ NS "keepassx" "keepassx" isKeepass nonFloating
-    , NS "zeal" "zeal" isZeal nonFloating
+    [ NS "keepassxc" "keepassxc" isKeepass manageKeepass
+    , NS "zeal" "zeal" isZeal manageZeal
     ]
+  where
+    manageKeepass = customFloating $ W.RationalRect 0.6 0.6 0.3 0.3
+    manageZeal = nonFloating
 
 -- Layouts
 myLayoutHook =
     onWorkspace "4:IM" imLayout $
-    onWorkspace "5:vm" full $
-    -- onWorkspace "6:mov" full $
+    -- onWorkspace "5:vm" full $
+    onWorkspace "6:mov" full $
     onWorkspace "7:gs" full $
     standardLayouts
   where
@@ -107,12 +116,14 @@ myManageHook =
     <+> namedScratchpadManageHook myScratchpads
     where
         manageSpecific = composeAll
-            [ isFullscreen --> doFullFloat
+            -- [ isFullscreen --> doFullFloat
+            [ isFullscreen --> (doF W.focusDown <+> doFullFloat)
+            -- , isKeepass --> doCenterFloat
             , className =? "Gimp" --> doShift "9:gimp"
             , className =? "Pidgin" --> doShift "4:IM"
             , className =? "Skype" --> doShift "4:IM"
             , className =? "Steam" --> doShift "7:gs"
-            , className =? "Spotify" --> doShift "8:music"
+            , className =? "spotify" --> doShift "8:music"
             , className =? "xfreerdp" --> doShift "5:vm"
             , className =? "vlc" --> doShift "6:mov"
             , className =? "Chromium-browser" --> doShift "6:mov"
